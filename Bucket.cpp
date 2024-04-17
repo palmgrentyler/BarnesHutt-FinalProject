89437440c0bdde82390e08cc6e418825a6e99c4a
palmgrentyler <110557606+palmgrentyler@users.noreply.github.com>
2024-04-16 17:20:23 -0700

    Update Bucket.cpp

--- Bucket.cpp
+++ Bucket.cpp
@@ -42,7 +42,6 @@ void Bucket::barnesHuttSort(Bucket *_parent, Point *point_list, unsigned num_poi
 		sort_buckets[i].indices = new unsigned[_parent->num_indices];
 		sort_buckets[i].num_indices = 0;
 	}
-
 	sort_buckets[0].min = _parent->min;
 	sort_buckets[0].max = mid;
 
@@ -60,22 +59,36 @@ void Bucket::barnesHuttSort(Bucket *_parent, Point *point_list, unsigned num_poi
 	for (unsigned i = 0; i < _parent->num_indices; i++) {
 		unsigned point_index;
 
-		point_index = indices[i];
+		point_index = _parent->indices[i];
 
 		sort_index = point_list[point_index].xCor > mid.xCor;
 		sort_index |= (point_list[point_index].yCor > mid.yCor) << 1;
 
-		Bucket* sort_bucket; 
+		printf("%p: bc: %d i: %d indices[%d] x,y: %f, %f, %d\n", _parent, _parent->num_indices, i, point_index, point_list[point_index].xCor, point_list[point_index].yCor, sort_index);
+		Bucket* sort_bucket;
 
 		sort_bucket = &sort_buckets[sort_index];
 
 		sort_bucket->indices[sort_bucket->num_indices] = point_index;
 		sort_bucket->num_indices++;
 	}
+	for (unsigned i = 0; i < 4; i++)
+	{
+		for (unsigned j = 0; j < sort_buckets[i].num_indices; j++)
+		{
+			printf("parent: %p bucket: %d indices[%d]: %d\n", _parent, i, j, sort_buckets[i].indices[j]);
+		}
+	}
+
+	printf("__________________________________\n");
 
 	for (int i = 0; i < 4; i++) {
 		if (sort_buckets[i].num_indices > 1) {
 			barnesHuttSort(&sort_buckets[i], point_list, num_points);
+		}
+		else if (sort_buckets[i].num_indices == 1) {
+			unsigned index = sort_buckets[i].indices[0];
+			printf("Solo: %p: bc: %d i: %d indices[%d] x,y: %f, %f\n", sort_buckets, sort_buckets[i].num_indices, i, i, point_list[index].xCor, point_list[index].yCor);
 		}
 	}
 }
